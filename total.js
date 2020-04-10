eb5af5353c03f2941768ab28681806e733b713cd
Mateus Paixão <mateus.paixao@corebiz.com.br>
2020-04-10 00:15:23 -0300

    first commit

---
+++ total.js
@@ -0,0 +1,22 @@
+// .cart-item-price__price span // lista https://store.playstation.com/pt-br/wishlist
+// .price-display__price // carrinho https://store.playstation.com/pt-br/checkout
+
+function total(className = '.cart-item-price__price span'){
+	const precos = document.querySelectorAll(className)
+
+	let total = 0
+
+	precos.forEach(real => {
+
+	let content = real.innerHTML.replace('R$', '').replace(',', '.')
+
+		if(content !== 'Gratuito'){
+			total = total + Number(content)
+		}
+	    
+	})
+
+	console.log(Intl.NumberFormat('pt-Br', { style: 'currency', currency: 'BRL' }).format(total))	
+}
+
+total()
